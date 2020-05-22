892233b823eb5367d4e157aba44fa6d68096f754
Yiqi Zhang <>
2020-05-22 19:27:33 -0400

    setup done

--- src/db/schema/development.sql
+++ src/db/schema/development.sql
@@ -1,7 +1,7 @@
 INSERT INTO users
   (user_id, games, gender)
 VALUES
-  ('adam', '{ "catan":{}, "cities":{}, "sea":{}, "puerto":{} }', 'male'),
+  ('adam', '{ "catan":[6,11,11,14], "cities":[2,0,0,1], "sea":[1,0,0,0], "puerto":[0,1,1,1] }', 'male'),
   ('eve', '{ "catan":{}, "cities":{}, "sea":{}, "puerto":{} }', 'female')
 
 
