e0b861a67a5b59766f8d1a371687e74c188cb0c5
Yiqi Zhang <>
2020-05-29 17:49:12 -0400

    added cors

--- src/application.js
+++ src/application.js
@@ -32,7 +32,23 @@ module.exports = function application(
   actions = { updateAppointment: () => {} }
 ) {
   app.use(cors());
+
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Origin",
+      "*,",
+      "https://catanscoreboard.herokuapp.com/api/users/adam"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token"
+    );
+    next();
+  });
+
   app.use(helmet());
+
   app.use(bodyparser.json());
 
   app.use("/api", users(db));
