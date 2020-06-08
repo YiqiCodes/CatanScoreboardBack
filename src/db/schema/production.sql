327d16d7a0fd92fab53bd3d1a005800ea6da09d4
Yiqi Zhang <>
2020-06-08 16:21:50 -0400

    changed production seeds

--- src/db/schema/production.sql
+++ src/db/schema/production.sql
@@ -1,8 +1,80 @@
-INSERT INTO users
-  (user_id, newgames, games, gender)
+INSERT INTO players
+  (id, name)
 VALUES
-  ('adam', '{{"10","6","4","5"}, {"3","4","6","10"}, {"6","10","6","8"}}', '{ "catantwo":[[10,6,4,5],[3,4,6,10], [6,10,6,8]], "catan":[6,11,11,14], "cities":[2,0,0,1], "sea":[1,0,0,0], "puerto":[0,1,1,1] }', 'male'),
-  ('eve', '{}', '{ "catan":{}, "cities":{}, "sea":{}, "puerto":{} }', 'female')
+  (DEFAULT, 'Dylan');
+INSERT INTO players
+  (id, name)
+VALUES
+  (DEFAULT, 'Mickias');
+INSERT INTO players
+  (id, name)
+VALUES
+  (DEFAULT, 'Rob');
+INSERT INTO players
+  (id, name)
+VALUES
+  (DEFAULT, 'Yiqi');
 
+INSERT INTO games
+  (id)
+VALUES
+  (DEFAULT);
+INSERT INTO games
+  (id)
+VALUES
+  (DEFAULT);
+INSERT INTO games
+  (id)
+VALUES
+  (DEFAULT);
 
-  
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 1, 6, 1);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 2, 10, 1);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 3, 6, 1);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 4, 8, 1);
+
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 1, 3, 2);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 2, 4, 2);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 3, 6, 2);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 4, 10, 2);
+
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 1, 10, 3);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 2, 6, 3);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 3, 4, 3);
+INSERT INTO game_details
+  (id, player_id, score, game_id)
+VALUES
+  (DEFAULT, 4, 5, 3);
